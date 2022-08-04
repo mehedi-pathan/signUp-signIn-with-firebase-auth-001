0e61f551e8f20b8b8aeacdc6550e2fb86bef958e
Md Mehedi Pathan <mehedi_pathan@Mehedis-MacBook.local>
2022-08-04 12:01:34 +0600

    added image to home

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,7 +1,12 @@
-import React from "react";
-
+import waitngImg from "../../assets/waiting_img.svg";
+import "./homePage.scss";
 const HomePage = () => {
-  return <div>Go to 'Registration' and enjoy further features</div>;
+  return (
+    <div className="homePage">
+      <img src={waitngImg} alt="waiting-image" className="home_img" />
+      <p className="text">Go to 'Registration' and enjoy further features</p>
+    </div>
+  );
 };
 
 export default HomePage;
